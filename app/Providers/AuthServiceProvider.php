f301270a1af2c29711ac21d94d1df8de44150344
Jorge Emanoel de Oliveira Pereira <eu.jorge.emanoell@gmail.com>
2020-01-14 01:22:20 -0300

    adiciona gates ao AuthServicePRovider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Module;
+use App\Acl\AclManager;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,41 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Gate::before(function ($user, $ability) {
+        //     if ($user->isSuperAdmin()) {
+        //         return true;
+        //     }
+        // });
+
+        // DEFINE GATES FOR ACL MANAGER
+        $modules = AclManager::allModules();
+
+        foreach ($modules as $module) {
+            foreach ($module->getActions() as $action_name) {
+                $gate = "{$module->name}.$action_name";
+                Gate::define($gate, function ($user, $model = null) use ($action_name, $module) {
+                    foreach ($module->getLevels() as $level_name) {
+                        $db_action_name = "$level_name.$action_name";
+
+                        if (!$module->hasPermission($db_action_name)) {
+                            return false;
+                        }
+
+                        foreach ($user->profiles as $profile) {
+                            if (!$module->profileHasPermission($profile, $db_action_name)) {
+                                return false;
+                            }
+                        }
+
+                        $action = AclManager::getMappedAction($level_name);
+                        if ($module->$action($user, $model, $db_action_name)) {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                });
+            }
+        }
     }
 }
