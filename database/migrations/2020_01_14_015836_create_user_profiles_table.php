deb9c37532284325b36bcab0de3a360e87669308
Jorge Emanoel de Oliveira Pereira <eu.jorge.emanoell@gmail.com>
2020-01-14 01:23:53 -0300

    cria migration user_profiles

---
+++ database/migrations/2020_01_14_015836_create_user_profiles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('profile_id');
+            $table->foreign('profile_id')
+                ->references('id')
+                ->on('profiles')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->dropForeign('user_profiles_profile_id_foreign');
+            $table->dropForeign('user_profiles_user_id_foreign');
+        });
+        Schema::dropIfExists('user_profiles');
+    }
+}
