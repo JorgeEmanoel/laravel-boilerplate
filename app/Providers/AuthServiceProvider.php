b9c864e98f7d4dc1deb92f4cfde1ccb6b1d90f6b
Jorge Emanoel de Oliveira Pereira <eu.jorge.emanoell@gmail.com>
2020-02-23 22:21:09 -0300

    atualiza AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Module;
 use App\Acl\AclManager;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,40 +29,55 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        // Gate::before(function ($user, $ability) {
-        //     if ($user->isSuperAdmin()) {
-        //         return true;
-        //     }
-        // });
+        Gate::before(function ($user, $ability) {
+            if ($user->isSuperAdmin()) {
+                return true;
+            }
+        });
 
         // DEFINE GATES FOR ACL MANAGER
-        $modules = AclManager::allModules();
 
-        foreach ($modules as $module) {
-            foreach ($module->getActions() as $action_name) {
-                $gate = "{$module->name}.$action_name";
-                Gate::define($gate, function ($user, $model = null) use ($action_name, $module) {
-                    foreach ($module->getLevels() as $level_name) {
-                        $db_action_name = "$level_name.$action_name";
+        if (Schema::hasTable('modules')) {
+            $modules = AclManager::allModules();
 
-                        if (!$module->hasPermission($db_action_name)) {
-                            return false;
-                        }
+            foreach ($modules as $module) {
+                foreach ($module->getActions() as $action_name) {
+                    $gate = "{$module->name}.$action_name";
+                    Gate::define($gate, function ($user, $model = null) use ($action_name, $module) {
+                        foreach ($module->getLevels() as $level_name) {
+                            $db_action_name = "$level_name.$action_name";
 
-                        foreach ($user->profiles as $profile) {
-                            if (!$module->profileHasPermission($profile, $db_action_name)) {
+                            if ($module->isAdminOnly()) {
                                 return false;
                             }
+
+                            if (!$module->hasPermission($db_action_name)) {
+                                return false;
+                            }
+
+                            if (!$user->profiles->count()) {
+                                return false;
+                            }
+
+                            $return = false;
+                            foreach ($user->profiles as $profile) {
+                                if ($module->profileHasPermission($profile, $db_action_name)) {
+                                    $return = true;
+                                }
+                            }
+
+                            if ($return) {
+                                $action = AclManager::getMappedAction($level_name);
+                                if ($module->$action($user, $model, $db_action_name)) {
+                                    return true;
+                                }
+                            }
+
                         }
 
-                        $action = AclManager::getMappedAction($level_name);
-                        if ($module->$action($user, $model, $db_action_name)) {
-                            return true;
-                        }
-                    }
-
-                    return false;
-                });
+                        return false;
+                    });
+                }
             }
         }
     }
